1909966f2740d3d606133016254540687d824ef1
srimantahyland <srimanta.singh@hyland.com>
2022-06-08 10:40:16 +0530

    Ran the following : #!/usr/bin/env bash mvn -U io.quarkus:quarkus-maven-plugin:create \         -DprojectGroupId=org.agoncal.quarkus.starting \         -DprojectArtifactId=rest-book \         -DclassName="org.agoncal.quarkus.starting.BookResource" \         -Dpath="/api/books" \         -Dextensions="resteasy-jsonb"

---
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
@@ -0,0 +1,9 @@
+package org.agoncal.quarkus.starting;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class BookResourceIT extends BookResourceTest {
+
+    // Execute the same tests but in native mode.
+}
